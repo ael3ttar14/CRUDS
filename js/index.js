aaf7021f08e201ccc0a42c869b33db9f7c9339db
ael3ttar14 <ahmed.el3ttar14@example.com>
2025-12-24 23:36:08 +0200

    CRUDS

---
+++ js/index.js
@@ -0,0 +1,130 @@
+let productName = document.getElementById('productName');
+let productPrice = document.getElementById('productPrice');
+let productCategory = document.getElementById('productCategory');
+let productDesc = document.getElementById('productDesc');
+let productList = [];
+let currentIndex= "";
+
+if(localStorage.getItem('products') != null){
+    productList = JSON.parse(localStorage.getItem('products'));
+    showProducts(productList);
+}
+
+function addProduct() {
+    let product = {
+        name : productName.value,
+        price : productPrice.value,
+        category : productCategory.value,
+        desc : productDesc.value
+    };
+    productList.push(product);
+    localStorage.setItem('products', JSON.stringify(productList)); 
+    showProducts();
+    clearForm();
+    showToast('Product Added!');
+
+}
+
+function clearForm(){
+    productName.value = '';
+    productPrice.value = '';
+    productCategory.value = '';
+    productDesc.value = '';
+ }
+
+function showProducts(list = productList) {
+    let temp = '';
+    for (let i = 0; i < list.length; i++) {
+        temp += `<tr class="new-row">
+            <td>${i + 1}</td>
+            <td>${list[i].name}</td>
+            <td>${list[i].price}</td>
+            <td>${list[i].category}</td>
+            <td>${list[i].desc}</td>
+            <td>
+                <button onclick="setFormForUpdate(${i})" class="btn btn-warning btn-sm action-btn" data-bs-toggle="tooltip" data-bs-title="Edit Product">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+            </td>
+            <td>
+                <button onclick="deleteProduct(${i})" class="btn btn-danger btn-sm action-btn" data-bs-toggle="tooltip" data-bs-title="Delete Product">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </td>
+        </tr>`;
+    }
+    document.getElementById('tableBody').innerHTML = temp;
+}
+
+
+
+ function deleteProduct(index){
+    productList.splice(index, 1);
+    localStorage.setItem('products', JSON.stringify(productList));
+    showProducts(productList);
+    showToast('Product Deleted!');
+
+}
+let matchedProducts = [];
+
+function searchProducts(term) {
+    let matchedProducts = [];    
+    for(let i = 0; i < productList.length; i++){
+        if(productList[i].name.toLowerCase().includes(term.toLowerCase())){
+            matchedProducts.push(productList[i]);
+        }
+    }   
+    showProducts(matchedProducts);
+}
+function setFormForUpdate(index){
+    currentIndex = index;
+    addBtn.classList.replace("d-block","d-none");
+    updateBtn.classList.replace("d-none","d-block");
+    productName.value = productList[index].name;
+    productPrice.value = productList[index].price;
+    productCategory.value = productList[index].category;
+    productDesc.value = productList[index].desc;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+}
+function returnAdd(){
+    addBtn.classList.replace("d-none","d-block");
+    updateBtn.classList.replace("d-block","d-none");
+}
+function updateProduct() {
+    let product = {
+        name : productName.value,
+        price : productPrice.value,
+        category : productCategory.value,
+        desc : productDesc.value
+    };
+    productList.splice(currentIndex, 1, product);
+    localStorage.setItem('products', JSON.stringify(productList)); 
+    showProducts();
+    clearForm();
+    returnAdd()
+    showToast('Product Updated!');
+}
+
+let scrollTopBtn = document.getElementById("scrollTopBtn");
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+scrollTopBtn.onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+
+function showToast(message, duration = 3000) {
+  let toast = document.getElementById('toast');
+  toast.textContent = message;
+  toast.classList.add('show');
+
+  setTimeout(() => {
+    toast.classList.remove('show');
+  }, duration);
+}
